e520ee0aedf743ae4c11c90554919f090b2ee960
Srihitha <saisrihithay@gmail.com>
2021-02-18 23:01:54 -0500

    chnages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
-import * as serviceWorker from './serviceWorker';
 
 import CourseManagerComponent from "./components/CourseManagerComponent";
 
@@ -11,5 +10,3 @@ ReactDOM.render(
     <CourseManagerComponent/>,
     document.getElementById('root')
 );
-
-serviceWorker.unregister();
